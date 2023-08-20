b40eb24b509b880fe0d3567dbbc8fa585daa16fd
esteban-vm <estebanvm90@outlook.com>
2023-08-20 10:06:35 -0400

    refactors

--- src/objects/background.ts
+++ src/objects/background.ts
@@ -3,10 +3,6 @@ import Layer from '@/layer'
 
 export default class Background implements DynamicObject {
   public game
-  private image1
-  private image2
-  private image3
-  private image4
   public layer1
   public layer2
   public layer3
@@ -15,14 +11,14 @@ export default class Background implements DynamicObject {
 
   constructor(game: Game) {
     this.game = game
-    this.image1 = document.getElementById('layer1') as HTMLImageElement
-    this.image2 = document.getElementById('layer2') as HTMLImageElement
-    this.image3 = document.getElementById('layer3') as HTMLImageElement
-    this.image4 = document.getElementById('layer4') as HTMLImageElement
-    this.layer1 = new Layer(this.game, this.image1, 0.2)
-    this.layer2 = new Layer(this.game, this.image2, 0.4)
-    this.layer3 = new Layer(this.game, this.image3, 1)
-    this.layer4 = new Layer(this.game, this.image4, 1.5)
+    const image1 = document.getElementById('layer1') as HTMLImageElement
+    const image2 = document.getElementById('layer2') as HTMLImageElement
+    const image3 = document.getElementById('layer3') as HTMLImageElement
+    const image4 = document.getElementById('layer4') as HTMLImageElement
+    this.layer1 = new Layer(this.game, image1, 0.2)
+    this.layer2 = new Layer(this.game, image2, 0.4)
+    this.layer3 = new Layer(this.game, image3, 1)
+    this.layer4 = new Layer(this.game, image4, 1.5)
     this.layers = [this.layer1, this.layer2, this.layer3]
   }
 
